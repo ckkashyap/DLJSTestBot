59ff5a417c610652f5da5b6aadbb2265df471df0
CK Kashyap <ckk@microsoft.com>
2019-10-13 12:39:55 -0700

    Added token creation logic

---
+++ Controllers/TokenController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Microsoft.BotBuilderSamples.Controllers
+{
+    [Route("tokens/generate")]
+    [ApiController]
+    public class TokenController : ControllerBase
+    {
+
+        [HttpGet]
+        public  async Task<string> GetAsync()
+        {
+            var secret = System.Environment.GetEnvironmentVariable("DIRECTLINE_SECRET");
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", secret);
+            var content = new StringContent("{\"User\" : {\"Id\": \"dl_ABCD\"}}", Encoding.UTF8, "application/json");
+            var response = await client.PostAsync("https://directline.botframework.com/v3/directline/tokens/generate", content);
+            return await response.Content.ReadAsStringAsync();
+        }
+
+    }
+    class JsonContent : HttpContent
+    {
+
+        private readonly MemoryStream _Stream = new MemoryStream();
+        public JsonContent(object value)
+        {
+
+            Headers.ContentType = new MediaTypeHeaderValue("application/json");
+            var jw = new JsonTextWriter(new StreamWriter(_Stream));
+            jw.Formatting = Formatting.Indented;
+            var serializer = new JsonSerializer();
+            serializer.Serialize(jw, value);
+            jw.Flush();
+            _Stream.Position = 0;
+
+        }
+        protected override Task SerializeToStreamAsync(Stream stream, TransportContext context)
+        {
+            return _Stream.CopyToAsync(stream);
+        }
+
+
+        protected override bool TryComputeLength(out long length)
+        {
+            length = _Stream.Length;
+            return true;
+        }
+    }
+}
